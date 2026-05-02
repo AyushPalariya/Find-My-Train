9c7c3e1a683f604fdc63582ac70014217f6fd3cd
Ayush <ayushpalariya96@gmail.com>
2026-05-02 11:09:42 +0530

    suggestion feature add

--- TrainUI/src/main/java/TT/Company/Train/contoller/Test.java
+++ TrainUI/src/main/java/TT/Company/Train/contoller/Test.java
@@ -7,6 +7,7 @@ import TT.Company.Train.entity.Station;
 import TT.Company.Train.entity.Train;
 import TT.Company.Train.entity.TrainSchedule;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/test")
-
+@CrossOrigin
 public class Test {
     @Autowired
     private StationRepo stationRepo;
